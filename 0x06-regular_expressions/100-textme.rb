8a1b891a92e3635b9631c39fb0ed9ab1b459dfb0
TMcMac <timmcmackenjr@gmail.com>
2020-11-09 18:28:45 +0000

    Cleans up the flags regex

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 from = ARGV[0].scan(/from:[+?a-zA-Z0-9]+/).join()
 to = ARGV[0].scan(/to:[+?a-zA-Z0-9]+/).join()
-flags = ARGV[0].scan(/flags:-?\d:-?\d:-?\d:-?\d:-?\d/).join()
+flags = ARGV[0].scan(/flags:[-:+0-9]*/).join()
 output = from[5..-1] + "," +  to[3..-1] + "," + flags[6..-1]
 puts output
