bf1b080ea55c6cff3df114d87337c80eb105d3a1
TMcMac <timmcmackenjr@gmail.com>
2021-02-04 21:03:07 +0000

    Gets 0 working

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+A simple script to query a given subreddit
+and return total subscribers or 0 if invalid
+"""
+
+import json
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Looks up numbers of subscribers to a subreddit"""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    pull = requests.get(url,
+                        headers={"user-agent": "user"},
+                        allow_redirects=False).json()
+    check = list(pull.keys())
+    if 'data' in check:
+        subs = pull.get('data').get('subscribers')
+    else:
+        subs = 0
+    return subs
