a4ebe98146359d903f7fa1cd71b18e6d0861e930
TMcMac <timmcmackenjr@gmail.com>
2021-02-05 02:24:20 +0000

    Adds exercise 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""
+A simple script to query a given subreddit
+and return 10 recent 'hot posts or none if invalid
+"""
+
+import json
+import requests
+
+
+def top_ten(subreddit):
+    """Looks up 10 recent hot posts from a subreddit"""
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    pull = requests.get(url,
+                        headers={"user-agent": "user"},
+                        allow_redirects=False).json()
+
+    if 'data' in pull:
+        data = pull.get('data').get('children')
+        for hot in data:
+            subdata = hot.get('data').get('title')
+            print(subdata)
+    else:
+        print('None')
+        return None
